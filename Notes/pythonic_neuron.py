9e206deb74ef3c161d3bffa8ebc258c759840bde
Pulin Kulshrestha <pulinkul5@gmail.com>
2026-03-24 22:51:46 +0530

    neuron simple py code

---
+++ Notes/pythonic_neuron.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+example_input = [1, 0.2, 0.1, 0.05, 0.2]
+example_weights = [0.2, 0.12, 0.4, 0.6, 0.90]
+
+input_vector = np.array(example_input)
+weight_vector = np.array(example_weights)
+bias_weight = 0.2
+
+activation_level = np.dot(input_vector, weight_vector) + (bias_weight * 1)
+print(activation_level)
+
+threshold = 0.5
+if activation_level > threshold:
+    perceptron_output = 1
+else:
+    perceptron_output = 0
+print(perceptron_output)
