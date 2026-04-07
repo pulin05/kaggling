6bca8ef7c0c025bfde64a323aedd8f1ec2636981
Pulin Kulshrestha <pulinkul5@gmail.com>
2026-04-07 22:20:22 +0530

    xaor keras v1

--- Notes/xor_keras.py
+++ Notes/xor_keras.py
@@ -7,4 +7,9 @@ x_train = np.array[[0, 0], [0, 1], [1, 0], [1, 1]]
 y_train = np.array[[0], [1], [1], [0]]
 
 model = Sequential()
-num_neurons
+num_neurons = 10
+model.add(Dense(num_neurons, input_dim=2))
+model.add(Activation("tanh"))
+model.add(Dense(1))
+model.add(Activation("sigmoid"))
+model.summary()
