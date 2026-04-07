4db2f4e559aeccea3441ec9a65bcb0cab6790ffc
Pulin Kulshrestha <pulinkul5@gmail.com>
2026-04-07 22:23:39 +0530

    fixed input and outupt dataset

--- Notes/xor_keras.py
+++ Notes/xor_keras.py
@@ -3,8 +3,8 @@ from keras import Sequential
 from keras.layers import Dense, Activation
 from keras.optimizers import SGD
 
-x_train = np.array[[0, 0], [0, 1], [1, 0], [1, 1]]
-y_train = np.array[[0], [1], [1], [0]]
+x_train = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y_train = np.array([[0], [1], [1], [0]])
 
 model = Sequential()
 num_neurons = 10
